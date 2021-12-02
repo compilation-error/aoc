10a65f9106364fafcf55f4c3e8e274288527e557
Romeet <romeetc@gmail.com>
2021-12-02 16:25:31 -0500

    complete day 1 solution

--- day1.py
+++ day1.py
@@ -6,7 +6,12 @@ def main():
     depths = pd.read_csv("day1_input.csv", header=None)
     dsign = np.sign(depths.tail(-1)[0].values - depths.head(-1)[0].values)
     dsum = np.sum(dsign[dsign > 0])
-    print(dsum)
+    print(f"part 1: {dsum}")
+
+    window = 3
+    dsign = np.sign(depths.tail(-window)[0].values - depths.head(-window)[0].values)
+    dsum = np.sum(dsign[dsign > 0])
+    print(f"part 2: {dsum}")
 
 
 if __name__ == "__main__":
